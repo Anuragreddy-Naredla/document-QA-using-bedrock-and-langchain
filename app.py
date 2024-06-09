90b8ed8717f734a4d96ec971313ff1dff09e2ae5
Anuragreddy-Naredla <69776971+Anuragreddy-Naredla@users.noreply.github.com>
2024-06-09 10:19:28 +0530

    updated_files

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-import json
 import os
-import sys
 import boto3
+import streamlit as st
 
 # to create the embeddings by using titan embedding model
 from langchain_community.embeddings import BedrockEmbeddings
@@ -18,6 +17,7 @@ from langchain.vectorstores import FAISS
 #LLM models
 from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
+os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'
 
 # bedrock client
 bedrock = boto3.client(service_name="bedrock-runtime")# it will access all the models
@@ -71,5 +71,48 @@ PROMPT = PromptTemplate(
     template=prompt_template, input_variables=["context", "question"]
 )
 
+def get_response_llm(llm, vectorstore_faiss, query):
+    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", 
+                                     retriever=vectorstore_faiss.as_retriever(search_type="similarity", search_kwargs={"k":3}),
+                                     return_source_documents=True, chain_type_kwargs={"prompt":PROMPT})
+    answer = qa({"query":query})
+    return answer['result']
+
+def main():
+    st.set_page_config("Chat PDF")
+    
+    st.header("Chat with PDF using AWS Bedrock💁")
+
+    user_question = st.text_input("Ask a Question from the PDF Files")
+
+    with st.sidebar:
+        st.title("Update Or Create Vector Store:")
+        
+        if st.button("Vectors Update"):
+            with st.spinner("Processing..."):
+                docs = data_ingestion()
+                get_vector_store(docs)
+                st.success("Done")
+
+    if st.button("Claude Output"):
+        with st.spinner("Processing..."):
+            faiss_index = FAISS.load_local("faiss_index", bedrock_embeddings)
+            llm=get_claude_llm()
+            
+            #faiss_index = get_vector_store(docs)
+            st.write(get_response_llm(llm,faiss_index,user_question))
+            st.success("Done")
+
+    if st.button("Llama2 Output"):
+        with st.spinner("Processing..."):
+            faiss_index = FAISS.load_local("faiss_index", bedrock_embeddings)
+            llm=get_llama2_llm()
+            
+            #faiss_index = get_vector_store(docs)
+            st.write(get_response_llm(llm,faiss_index,user_question))
+            st.success("Done")
+
+if __name__ == "__main__":
+    main()
 
 
